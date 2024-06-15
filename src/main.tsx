ca76c4665501c8974ff1e768bf2ea0882aadbb0b
thomasrichmond <thomas.richmond8@gmail.com>
2024-06-15 15:20:02 +1000

    Adding vercel analytics to site

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import Terminal from './Terminal.tsx'
-import './index.css'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import Terminal from "./Terminal.tsx";
+import "./index.css";
+import { Analytics } from "@vercel/analytics/react";
 
-
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Terminal />
-  </React.StrictMode>,
-)
+    <Analytics />
+  </React.StrictMode>
+);
